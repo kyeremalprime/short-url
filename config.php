36193627f01ea094adab8bd18c9d24f479f58213
kyeremal <kyeremalprime@gmail.com>
2016-05-02 02:45:05 +0800

    Update config.php

--- config.php
+++ config.php
@@ -1,9 +1,9 @@
 <?php
     $site_title = "Short URL";
     $site_url = "http://xx.wtf/";
-    $mysql_dbhost = "rds666o492k56iq5233zo.mysql.rds.aliyuncs.com";
+    $mysql_dbhost = "127.0.0.1";
     $mysql_dbname = "shorturl";
     $mysql_dbuser = "shorturluser";
-    $mysql_dbpassword = "zouyuqin";
+    $mysql_dbpassword = "shorturlpass";
     $mysql_dbport = "3306";
 ?>
