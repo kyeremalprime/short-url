ad1767d0b8d1d1eb9244fd4f33baaed2b63c32e0
kyeremal <kyeremalprime@gmail.com>
2016-05-02 05:03:36 +0800

    Update go.php

--- go.php
+++ go.php
@@ -3,9 +3,9 @@
     function GetLongUrl($short_url) {
         include 'config.php';
         $db = new PDO('mysql:host=' . $mysql_dbhost . ';dbname=' . $mysql_dbname . ';port=' . $mysql_dbport, $mysql_dbuser, $mysql_dbpassword);
-	    $db ->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);	
-	    $db ->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE , PDO::FETCH_ASSOC );
-	    $sql = $db->prepare('SELECT * FROM rnyz_urlinfo WHERE short_url = "'.$short_url.'"');
+	$db ->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);	
+	$db ->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE , PDO::FETCH_ASSOC );
+	$sql = $db->prepare('SELECT * FROM ' . $mysql_table . ' WHERE short_url = "'.$short_url.'"');
         $sql->execute();
         $res = $sql->fetch(PDO::FETCH_ASSOC);
 	    $db = null;
